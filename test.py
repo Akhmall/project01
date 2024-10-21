59680dc09eb2106d7264b17f07f30c19c41a9cbd
M.AKHMAL <137740587+Akhmall@users.noreply.github.com>
2024-10-21 19:56:14 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+first_name = 'm'
+last_name = 'akmal'
+full_name = f'{first_name} {last_name}'
+print(full_name)
+
+from datetime import datetime
+today = datetime.now()
+print(today)
+
+from datetime import datetime
+today = datetime.now()
+
+date_time = today.strftime("%Y-%M-%d %H-%M-%S")
+print(date_time)
